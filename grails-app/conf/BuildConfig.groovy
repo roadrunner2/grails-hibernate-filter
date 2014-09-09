f899cce834771255c687eb51a1b2fa7dadbc4222
Matt Bugbee <matt.bugbee@rtslabs.com>
2014-09-09 11:41:37 -0400

    upgraded to Grails 2.4.3, updated dependencies, and bumped version to 0.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,20 +1,29 @@
 grails.project.work.dir = 'target'
-grails.project.source.level = 1.6
+
+grails.project.dependency.resolver = 'maven'
 
 grails.project.dependency.resolution = {
+    inherits 'global'
+    log 'warn'
 
-	inherits 'global'
-	log 'warn'
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
 
-	repositories {
-		grailsCentral()
-	}
+    dependencies {
+    }
 
-	dependencies {}
-
-	plugins {
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
-			export = false
-		}
-	}
+    plugins {
+        build (":release:3.0.1") {
+            export = false
+        }
+        build (":rest-client-builder:1.0.2") {
+            export = false
+        }
+        compile (":hibernate4:4.3.5.5") {
+            export = false
+        }
+    }
 }
